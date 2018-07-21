d254fe148c3f38da327d181292c2968e76658bcd
Peteypete <peter@peterwen.com>
2018-07-21 09:13:58 -0700

    working wto versions w adapt-react-class

--- project.clj
+++ project.clj
@@ -73,7 +73,7 @@
 
   :figwheel
   {:http-server-root "public"
-   :server-port 3450
+   :server-port 3451
    :nrepl-port 7002
    :nrepl-middleware ["cemerick.piggieback/wrap-cljs-repl"
                       ]
--- src/cljs/reagent_react_select/core.cljs
+++ src/cljs/reagent_react_select/core.cljs
@@ -1,5 +1,5 @@
 (ns reagent-react-select.core
-    (:require [reagent.core :as r :refer [atom]]
+    (:require [reagent.core :as r]
               [secretary.core :as secretary :include-macros true]
               [accountant.core :as accountant]
               [cljsjs.classnames]
@@ -8,7 +8,49 @@
 
 
 (defonce value (r/atom nil))
+
+; Version 1 --------------
+(defn select
+  "Select based on a atom/cursor. Pass as state"
+  [{:keys [state]
+    :as props}]
+  [(r/adapt-react-class js/Select.Async)
+   (-> props
+       (dissoc state)
+       (assoc :value @state
+              :on-change #(reset! state (aget % "value"))))])
+
+(defonce !state (atom nil))
+
+(defn load-options [input cb]
+  ;; return sample
+  (cb nil #js{:options (->> (range 20)
+                            (map (fn [item]
+                                   {:value item
+                                    :label (str item)}))
+                            clj->js)
+              :complete true}))
+(defn root-ui []
+  [select {:state !state
+           :multi true
+           :load-options load-options}])
+
+;; Version 2--------------
+
+(defn select-ui2 []
+  [(r/adapt-react-class js/window.Select) {:value @value
+                        :options #js [#js {:value "a" :label "alpha"}
+                                      #js {:value "b" :label "beta"}
+                                      #js {:value "c" :label "gamma"}]
+                        :onChange #(reset! value (aget % "value"))}])
+
+(defn root-ui2 []
+  [:div {:style {:width 400}}
+   [:h3 "Select test 3"]
+   [select-ui2]])
+
 ;; -------------------------
+
 ;; Views
 
 (defn home-page []
@@ -19,25 +61,16 @@
      "https://cdnjs.cloudflare.com/ajax/libs/react-select/1.2.1/react-select.min.css",
      :rel "stylesheet"}]
    [root-ui]
+   [root-ui2]
    [:div [:a {:href "/about"} "go to about page"]]])
 
 (defn about-page []
   [:div [:h2 "About reagent-react-select"]
    [:div [:a {:href "/"} "go to the home page"]]])
 
-(defn select-ui []
-  [:> js/window.Select {:value @value
-                        :options #js [#js {:value "a" :label "alpha"}
-                                      #js {:value "b" :label "beta"}
-                                      #js {:value "c" :label "gamma"}]
-                        :onChange #(reset! value (aget % "value"))}])
 
-(defn root-ui []
-  [:div {:style {:width 400}}
-   [:h3 "Select test"]
-   [select-ui]])
 
-;; -------------------------
+
 ;; Routes
 
 (defonce page (atom #'home-page))
